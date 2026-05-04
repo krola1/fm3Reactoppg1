38bfa3a55a670d1c105204e52249aed9330a3bb8
krola1 <kwl082024@gmail.com>
2026-05-04 14:35:30 +0200

    bang

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FM3REACTOPPG1/",
+  base: "/fm3Reactoppg1/",
 });
